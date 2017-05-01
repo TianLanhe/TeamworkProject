f96e60941269119adfb3320ee41f012795f0dc5b
Alex <aptxx_4869@163.com>
2017-05-01 22:12:10 +0800

    添加了XML文件过滤器

--- src/teamwork/listener/LoadFileListener.java
+++ src/teamwork/listener/LoadFileListener.java
@@ -3,9 +3,9 @@ package teamwork.listener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.filechooser.FileFilter;
 
 import teamwork.controler.NewsLoadingControler;
 import teamwork.loader.NewsLoader;
@@ -14,6 +14,7 @@ public class LoadFileListener implements ActionListener {
   public void actionPerformed(ActionEvent e) {
     JFileChooser jfc = new JFileChooser();
     jfc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+    jfc.addChoosableFileFilter(new XmlFileFilter("xml"));
 
     int state = jfc.showOpenDialog(null);
     if (state == JFileChooser.APPROVE_OPTION) {
@@ -24,5 +25,42 @@ public class LoadFileListener implements ActionListener {
         JOptionPane.showMessageDialog(null, "文件格式错误！", "错误", JOptionPane.ERROR_MESSAGE);
       }
     }
+    
   }
+}
+class XmlFileFilter extends FileFilter{
+  
+  String xml;
+  
+  public XmlFileFilter(String xml) {
+    // TODO Auto-generated constructor stub
+    this.xml = xml;
+  }
+
+  @Override
+  public boolean accept(File f) {
+    // TODO Auto-generated method stub
+    if (f.isDirectory()) {
+      return true;
+    }
+    String fileName = f.getName();
+    int index = fileName.lastIndexOf('.');
+    if (index > 0 &&  index < fileName.length() - 1) {
+      String extension = fileName.substring(index + 1).toLowerCase();
+      if (extension.equals(xml)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Override
+  public String getDescription() {
+    // TODO Auto-generated method stub
+    if (xml.equals("xml")) {
+      return "*.xml";
+    }
+    return "";
+  }
+  
 }
