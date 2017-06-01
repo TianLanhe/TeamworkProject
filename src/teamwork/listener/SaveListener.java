7c83fc065697c93c415009af0d9a0a1feb0a6315
hb_weng <hb_weng@163.com>
2017-06-01 23:44:24 +0800

    进度保存设置密码

--- src/teamwork/listener/SaveListener.java
+++ src/teamwork/listener/SaveListener.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
-import javax.swing.filechooser.FileFilter;
+import javax.swing.JPasswordField;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 
@@ -17,6 +17,8 @@ public class SaveListener implements ActionListener {
 
   private BufferedWriter out;
   private FileWriter fileWriter;
+  private int returnVal;
+  private char[] pwd;
 
   @Override
   public void actionPerformed(ActionEvent e) {
@@ -27,7 +29,18 @@ public class SaveListener implements ActionListener {
       "进度文件(*.mmp)", "mmp");  
     jfc.setFileFilter(filter); 
     jfc.setCurrentDirectory(jfc.getSelectedFile());
-    //jfc.addChoosableFileFilter(new MmpFileFilter("进度文件(*.mmp)"));
+    
+    returnVal = jfc.showOpenDialog(null);
+    if (returnVal == JFileChooser.APPROVE_OPTION) {
+      JPasswordField pw = new JPasswordField();
+      JOptionPane.showMessageDialog(null, pw, "请设定6位密码", JOptionPane.PLAIN_MESSAGE);
+      
+      pwd = pw.getPassword();
+      while(pwd.length != 6){
+        JOptionPane.showMessageDialog(null,pw,"密码必须为6位", JOptionPane.PLAIN_MESSAGE);
+        pwd = pw.getPassword();
+        }
+    }
     
     int state = jfc.showSaveDialog(null);
     if (state == JFileChooser.APPROVE_OPTION) {
@@ -35,16 +48,18 @@ public class SaveListener implements ActionListener {
         File dir = jfc.getCurrentDirectory();
         String name = jfc.getSelectedFile().getName();
         File file = new File(dir, name);
+        Object[] options = {"是的","取消"};
        
         if(file.exists()){//已存在文件
-          JOptionPane.showConfirmDialog(null, file + "文件已经存在,是否覆盖?", "文件存在", JOptionPane.YES_NO_OPTION,
-            JOptionPane.WARNING_MESSAGE);//显示一个对话框来实现是否覆盖源文件
+          JOptionPane.showOptionDialog(null, file + "文件已经存在,是否覆盖?", "文件存在", JOptionPane.YES_NO_OPTION,
+            JOptionPane.WARNING_MESSAGE,null,options,options[0]);//显示一个对话框来实现是否覆盖源文件
         }
         
         if(name.indexOf(".mmp")==-1){
           file = new File(jfc.getCurrentDirectory(),name+".mmp");
         }
        
+        
         fileWriter = new FileWriter(file);
         out = new BufferedWriter(fileWriter);
         out.close();
